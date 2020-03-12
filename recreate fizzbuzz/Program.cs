6b62b9f4e9a9e33a567b1ef8ba872604f356a9ac
cristianmar25 <cri.mar214@gmail.com>
2020-03-11 21:11:42 -0700

    Add project files.

---
+++ recreate fizzbuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace recreate_fizzbuzz
+{
+    class Program
+    {
+        private const int V = 100;
+
+        static void Main(string[] args)
+        {
+            // TODO: tengo que agregar eso. 
+            for (int i = 1; i
+                < V; i++)
+            {
+                string res = (i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" : (i % 5 == 0) ? "Fizz" : (i % 3 == 0) ? "Buzz" : i.ToString();
+                Console.WriteLine(res);
+            }
+            Console.ReadLine();
+        }
+    }
+}
